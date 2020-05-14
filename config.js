442415f5bde6bfcd1ab01b37b83a698b24562c18
Andrew Stone <g.andrew.stone@gmail.com>
2020-05-14 13:31:47 -0400

    add server configuration file

---
+++ config.js
@@ -0,0 +1,9 @@
+
+
+exports.REPO_URL = "ssh:git@gitlab.com:nextchain/www.git";  // "https://github.com/bitcoin-unlimited/BUwiki"
+exports.PUSH_BRANCHES = ["refs/heads/master:refs/heads/master"];
+exports.UPSTREAM_REPO_NAME = "origin";
+exports.REPO_BRANCH_NAME = "master";
+
+exports.STACKEDIT_URL = "http://192.168.1.100:8080/app";
+// exports.STACKEDIT_URL = "https://stackedit.io/app";
